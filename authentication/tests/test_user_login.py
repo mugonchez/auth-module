f1309d02a8866e6235937fcf85d608ed70816048
moses-mugoya <mosesmugoya31@gmail.com>
2024-03-07 10:36:51 +0300

    added unit tests for user login

--- authentication/tests/test_user_login.py
+++ authentication/tests/test_user_login.py
@@ -1,12 +1,47 @@
-import json
 from django.test import TestCase
 from authentication.models import User
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
-from authentication.utils import generate_token
-from django.core import mail
-from django.conf import settings
+
+
+class TestUserLogin(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.login_url = reverse('authentication:login')
+
+        self.user = User.objects.create_user(
+            email = 'john@gmail.com',
+            first_name= 'John',
+            last_name = 'Doe',
+            username = 'Johntez',
+            phone_number= '0712345678',
+            password = '@Rand0mpassword'
+        )
+
+    
+    def test_valid_user_login(self):
+        data = {
+            'email':'john@gmail.com',
+            'password': '@Rand0mpassword'
+        }
+        response = self.client.post(self.login_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+
+    def test_invalid_user_login(self):
+        data = {
+            'email':'john@gmail.com',
+            'password': 'wrongpassword' # validate wrong password
+        }
+        response = self.client.post(self.login_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+
+
+
+    
+
+
